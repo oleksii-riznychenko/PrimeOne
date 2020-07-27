6ac7252eda4f2f5ff37a53a9ded05ca75961900f
Алексей Резниченко <ReznichenkoAlex@MacBook-Pro-Aleksej.local>
2020-07-27 20:10:51 +0300

    PrimeOne

--- #source/js/script.js
+++ #source/js/script.js
@@ -18,7 +18,7 @@ if (isMobile.iOS()) {
 
 let iconMenu = document.querySelector(".icon-menu");
 let body = document.querySelector("body");
-let menuBody = document.querySelector(".menu__body");
+let menuBody = document.querySelector(".header-menu");
 if (iconMenu) {
 	iconMenu.addEventListener("click", function () {
 		iconMenu.classList.toggle("active");
